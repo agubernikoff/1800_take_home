20300a4cea6837a534bbce33c9014da339d569ba
alexander gubernikoff <agubernikoff@gmail.com>
2022-09-26 13:35:42 -0400

    autocomplete

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { postsActions } from "./store/posts-slice";
 
 function Search() {
+  const posts = useSelector((state) => state.posts.posts);
+
+  const options = [...posts].map((p) => (
+    <option key={p.id}>
+      {p.title
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ")}
+    </option>
+  ));
+
   const dispatch = useDispatch();
 
   const [searchText, setSearchText] = useState("");
@@ -15,10 +26,12 @@ function Search() {
       <label>Search for a post by title: </label>
       <input
         type="search"
+        list="options"
         size="70"
         value={searchText}
         onChange={(e) => setSearchText(e.target.value)}
       ></input>
+      <datalist id="options">{options}</datalist>
     </div>
   );
 }
